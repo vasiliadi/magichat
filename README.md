34f888e0a6c1dca6ce01fe676d91e53d92f15bdd
vasiliadi <9432426+vasiliadi@users.noreply.github.com>
2023-12-21 15:43:34 -0600

    Init commit.

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# magichat
-Magic Hat for Secret Santa
+# Magic Hat for Secret Santa
+
+## Initial members
+
+`db.xlsx` used for initial databese creation. \
+You **need to edit** with your participants. \
+You can edit **only yellow rows**. \
+If you want to gift family member or all participants from different families just type for all differeft number in `family` column.
+
+## PostgreSQL configuration
+
+Edit `secrets.toml` in `.streamlit` folder and modify with your data.
+
+Example of config:
+
+```
+[connections.postgresql]
+dialect = "postgresql"
+host = "xxxxxx"
+port = "5432"
+database = "xxxxxx"
+username = "xxxxxx"
+password = "xxxxxx"
+```
+
+where `host`, `port`, `database`, `username` and `password` replace with yours.
+
+Free PostgreSQL nodes avalible on
+[neon.tech](https://neon.tech/) or [elephantsql.com](https://www.elephantsql.com/) or wherever you want.
+
+## Deploy
+
+To deploy you need [streamlit.io](https://streamlit.io/) account and your own [github.com](https://github.com/) repository. Just fork it and replace `db.xlsx` and `secrets.toml` with you own.
---
+++ streamlit_app.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import random
+import pandas as pd
+from sqlalchemy import text
+
+conn = st.connection("postgresql-neon", type="sql")
+
+try:
+    pd.read_excel('db.xlsx', sheet_name='gifters').to_sql('gifters', conn.engine, index=False)
+    pd.read_excel('db.xlsx', sheet_name='gifters').to_sql('resiviers', conn.engine, index=False)
+    pd.read_excel('db.xlsx', sheet_name='results').to_sql('results', conn.engine, index=False)
+except ValueError:
+    pass
+
+gifters = pd.read_sql_table('gifters', conn.engine)
+resiviers = pd.read_sql_table('resiviers', conn.engine)
+conn.engine.dispose()
+
+st.title('Magic Hat 🎩')
+
+name = st.selectbox(
+    'Please select your name...',
+    gifters['name'].sort_values(),
+    index=None,
+    placeholder='Please select your name...', 
+    label_visibility='collapsed')
+
+if name != None:
+    st.text(f'Hi {name} 👋 ')
+    st.text('Press the button to know who you are gifting to:')
+    if st.button('Hocus Pocus 🪄', type='primary'):
+        choice = random.choice(resiviers['name'])
+        while (name == choice) or (gifters[gifters['name'] == name]['familyID'].isin(resiviers[resiviers['name'] == choice]['familyID']).iloc[0]):
+            choice = random.choice(resiviers['name'])
+
+        with conn.session as s:
+            s.execute(text('DELETE FROM gifters WHERE name = :name'), {'name': name})
+            s.execute(text('DELETE FROM resiviers WHERE name = :choice'), {'choice': choice})
+            s.execute(text('INSERT INTO results VALUES (:result)'), {'result': f'{name} is gifting to {choice}'})
+            s.commit()
+            st.header(f'{name} is gifting to {choice}')
+
+        st.balloons()
